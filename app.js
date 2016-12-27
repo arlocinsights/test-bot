6c5418173355993d46ca2827fbbedbaeb3b9825e
adi4info <araghavendra@4info.com>
2016-12-27 14:06:48 -0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,146 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening1 to %s', server.name, server.url); 
+});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    //appId: process.env.MICROSOFT_APP_ID,
+    //appId: 'c8968071-cbb0-4125-a186-996124f8d93f',
+    //appPassword: 'CEjeiFS7c2ackN2GhrxSf77'
+    appId: 'baf6a462-54d1-46da-af0f-ddd3693d5eeb',
+    appPassword: 'CYRLUgbtkybufrozBZ5T5d4'
+});
+
+//var connector = new builder.ConsoleConnector().listen();
+//var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+
+
+var bot = new builder.UniversalBot(connector, [
+    function (session) {
+        session.send("Hello... I'm a decision bot.");
+        console.log("Sending hello message")
+        session.beginDialog('rootMenu');
+    },
+    function (session, results) {
+        session.endConversation("Goodbye until next time...");
+    }
+]);
+
+bot.dialog('/', function (session) {
+	console.log("Message received")
+
+    session.send("Hello World!");
+    session.beginDialog('rootMenu');
+    });
+    
+// Add root menu dialog
+bot.dialog('rootMenu', [
+    function (session) {
+    	console.log("Sending choice options")
+        builder.Prompts.choice(session, "Choose an option:", 'Flip A Coin|Roll Dice|Magic 8-Ball|Quit');
+    },
+    function (session, results) {
+        switch (results.response.index) {
+            case 0:
+                session.beginDialog('flipCoinDialog');
+                break;
+            case 1:
+                session.beginDialog('rollDiceDialog');
+                break;
+            case 2:
+                session.beginDialog('magicBallDialog');
+                break;
+            default:
+                session.endDialog();
+                break;
+        }
+    },
+    function (session) {
+        // Reload menu
+        session.replaceDialog('rootMenu');
+    }
+]).reloadAction('showMenu', null, { matches: /^(menu|back)/i });
+
+// Flip a coin
+bot.dialog('flipCoinDialog', [
+    function (session, args) {
+        console.log("Flip coin dialog")
+        builder.Prompts.choice(session, "Choose heads or tails.", "heads|tails", { listStyle: builder.ListStyle.none })
+    },
+    function (session, results) {
+        var flip = Math.random() > 0.5 ? 'heads' : 'tails';
+        if (flip == results.response.entity) {
+            session.endDialog("It's %s. YOU WIN!", flip);
+        } else {
+            session.endDialog("Sorry... It was %s. you lost :(", flip);
+        }
+    }
+]);
+
+// Roll some dice
+bot.dialog('rollDiceDialog', [
+    function (session, args) {
+        builder.Prompts.number(session, "How many dice should I roll?");
+    },
+    function (session, results) {
+        if (results.response > 0) {
+            var msg = "I rolled:";
+            for (var i = 0; i < results.response; i++) {
+                var roll = Math.floor(Math.random() * 6) + 1;
+                msg += ' ' + roll.toString(); 
+            }
+            session.endDialog(msg);
+        } else {
+            session.endDialog("Ummm... Ok... I rolled air.");
+        }
+    }
+]);
+
+// Magic 8-Ball
+bot.dialog('magicBallDialog', [
+    function (session, args) {
+        builder.Prompts.text(session, "What is your question?");
+    },
+    function (session, results) {
+        // Use the SDK's built-in ability to pick a response at random.
+        session.endDialog(magicAnswers);
+    }
+]);
+
+var magicAnswers = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes, definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"
+];
